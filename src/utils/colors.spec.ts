beeb52dadfa6bb43abd60c7fff00a21cc8a05585
Daniele Gazzelloni <dangz.dev@gmail.com>
2025-12-16 09:31:11 +0000

    fix: tests for colors.spec

--- src/utils/colors.spec.ts
+++ src/utils/colors.spec.ts
@@ -1,10 +1,16 @@
-import { hexToRgb } from './colors';
+import { hexToRgbA } from './colors';
 
-describe('hexToRgb', () => {
-  it('should convert hex color to RGB tuple', () => {
-    expect(hexToRgb('#ff5733')).toEqual([255, 87, 51]);
-    expect(hexToRgb('#000000')).toEqual([0, 0, 0]);
-    expect(hexToRgb('#ffffff')).toEqual([255, 255, 255]);
-    expect(hexToRgb('#4a00f0')).toEqual([74, 0, 240]);
+describe('hexToRgbA', () => {
+  it('should convert hex color to RGBA tuple, with alpha set to 80% (204)', () => {
+    expect(hexToRgbA('#ff5733')).toEqual([255, 87, 51, 204]);
+    expect(hexToRgbA('#000000')).toEqual([0, 0, 0, 204]);
+    expect(hexToRgbA('#ffffff')).toEqual([255, 255, 255, 204]);
+    expect(hexToRgbA('#4a00f0')).toEqual([74, 0, 240, 204]);
+  });
+
+  it('should allow custom alpha value', () => {
+    expect(hexToRgbA('#ff5733', 255)).toEqual([255, 87, 51, 255]);
+    expect(hexToRgbA('#000000', 128)).toEqual([0, 0, 0, 128]);
+    expect(hexToRgbA('#ffffff', 0)).toEqual([255, 255, 255, 0]);
   });
 });
